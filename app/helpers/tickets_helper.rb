93ac340d848c160592fae9b5872df81379906da2
Evgeniy Zabolotniy <evgeniy.zabolotniy@gmail.com>
2014-02-08 22:45:15 +0200

    Added history page and navigation

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -1,2 +1,13 @@
 module TicketsHelper
+  def history_changes(ticket_version)
+    changes = (ticket_version.reify.changed & ['user_id', 'status']).map do |key|
+      if key == 'user_id'
+        "Ticket was assigned to #{ticket_version.reify.user.name}" 
+      else
+        "Status was changed to '#{t("tickets.statuses.#{ticket_version.reify.status}")}'"
+      end
+    end
+
+    "#{ticket_version.created_at.strftime('%m/%d/%Y %H:%M')}: #{changes.join(' and ')}"
+  end
 end
